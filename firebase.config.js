47ccf41f2e5d228bf01d991ac906678b4145ec2e
Gabriel F. <gfss1@aluno.ifal.edu.br>
2025-08-21 13:52:25 -0300

    up

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9tYvim1HqG35MbtBjRWSYhZd5ElVXAKs",
+  authDomain: "servicos-eg.firebaseapp.com",
+  projectId: "servicos-eg",
+  storageBucket: "servicos-eg.firebasestorage.app",
+  messagingSenderId: "659609116408",
+  appId: "1:659609116408:web:996bfdb87d7d22b2ed250d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
